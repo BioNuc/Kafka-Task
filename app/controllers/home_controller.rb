7b03fe0f3a7a0749f893260a09d7ce9cc7d7faff
SherifYosri <sherif0yosri@gmail.com>
2019-04-09 20:29:19 +0200

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
+  layout "application"
   def index
-  	WaterDrop::SyncProducer.call('message', topic: 'testTopic')
+  	
+  end
+
+  def create
+  	vote_manager.publish params
+  	flash[:success] = "Your response has been recorded."
+  	redirect_back fallback_location: { action: index }
+  end
+
+  private
+  
+  def vote_manager
+  	require_dependency 'vote_manager'
+  	VoteManager.new
   end
 end
